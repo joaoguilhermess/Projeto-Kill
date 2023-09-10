0efd890a3b0616e4dd11f45b2df68d0ea8aca6b4
Jotagê <71993919+JG-entusiasta@users.noreply.github.com>
2023-09-10 16:17:37 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-from time import sleep
-from os import path
+import os
+import subprocess
 import psutil
+import time
 
 class Kill:
 	@classmethod
 	def Init(self):
 		self.name = "kill.txt"
-		self.delay = 1
+		self.delay = 0.5
+
+		self.max = 5
+
+		start = time.time()
 
 		while True:
 			self.read()
 
-			self.kill()
+			self.killServices()
 
-			sleep(self.delay)
+			self.killProcesses()
+
+			if time.time() - start > self.max:
+				break
+
+			time.sleep(self.delay)
 
 	@classmethod
 	def read(self):
-		if not path.exists(self.name):
+		if not os.path.exists(self.name):
 			file = open(self.name, "w")
 			file.close()
 
@@ -26,21 +36,36 @@ class Kill:
 		self.list = file.read().split("\n")
 
 	@classmethod
-	def kill(self):
-		l = psutil.pids()
-		for pid in l:
+	def killServices(self):
+		for s in psutil.win_service_iter():
 			try:
-				p = psutil.Process(pid)
+				if s.status() == "running":
+					name = s.display_name()
 
+					p = psutil.Process(s.pid())
+
+					cmd = " ".join(p.cmdline())
+
+					if cmd in self.list or name in self.list:
+						subprocess.run(["net", "stop", str(s.name())], capture_output=True)
+						print(s.name(), name, cmd)
+			except Exception as e:
+				# print("error", e)
+				pass
+
+	@classmethod
+	def killProcesses(self):
+		for p in psutil.process_iter():
+			try:
 				name = p.name()
 
 				cmd = " ".join(p.cmdline())
 
 				if cmd in self.list or name in self.list:
-					p.kill()
+					subprocess.run(["taskkill", "/f", "/t", "/pid", str(p.pid)], capture_output=True)
 					print(name, cmd)
-
 			except Exception as e:
+				# print("error", e)
 				pass
 
 Kill.Init()
